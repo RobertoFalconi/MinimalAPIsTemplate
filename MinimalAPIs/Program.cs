111ab4f6b3a3f9d484ad66246ecb78768648fa4e
RobertoFalconi <falkons@outlook.com>
2022-07-09 10:24:39 +0200

    Add JwtBearerEvents

--- MinimalAPIs/Program.cs
+++ MinimalAPIs/Program.cs
@@ -4,11 +4,16 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = new X509SecurityKey(new X509Certificate2(builder.Configuration["Certificate:Path"], builder.Configuration["Certificate:Password"]));
-var connectionString = builder.Configuration["ConnectionString"];
+var connectionStringFromSecrets = builder.Configuration.GetConnectionString("connectionstring");
+var connectionStringFromAppsettings = builder.Configuration["ConnectionString"];
+//builder.Services.AddDbContext<yourDbContext>(options => options.UseSqlServer(yourConnectionString));
+
+//var key = new X509SecurityKey(new X509Certificate2(builder.Configuration["Certificate:Path"], builder.Configuration["Certificate:Password"]));
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
@@ -22,13 +27,23 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
         ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = key
     };
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = context =>
+        {
+            var prova = context?.Principal?.Claims?.FirstOrDefault(x => x.Type == "custom")?.Value;
+            if (prova == null)
+            {
+                context?.Fail("Unauthorized");
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
 builder.Services.AddAuthorization();
 
 builder.Logging.AddJsonConsole();
 builder.Services.AddHealthChecks();
-
-//builder.Services.AddDbContext<yourDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -61,9 +76,7 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-
 var app = builder.Build();
-
 
 app.UseHsts();
 app.UseHttpsRedirection();
@@ -87,8 +100,10 @@ else
 
 app.MapGet("/generateToken", () =>
 {
-    var jwtHeader = new JwtHeader(new SigningCredentials(key, SecurityAlgorithms.RsaSha256));
+    //var jwtHeader = new JwtHeader(new SigningCredentials(key, SecurityAlgorithms.RsaSha256));
+    var jwtHeader = new JwtHeader(new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature));
     var jwtPayload = new JwtPayload(builder.Configuration["Jwt:Issuer"], builder.Configuration["Jwt:Audience"], null, null, DateTime.Now.Add(new TimeSpan(0, 0, 1800)), null);
+    jwtPayload.AddClaim(new System.Security.Claims.Claim("custom", "prova"));
     return new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(jwtHeader, jwtPayload));
 });
 
@@ -99,5 +114,3 @@ app.MapGet("/error", () => "An error happened.");
 app.MapHealthChecks("/healthz");
 
 app.Run();
-
-public record JsonResponse(string ErrorMessage);
