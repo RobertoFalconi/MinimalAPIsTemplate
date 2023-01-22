8331f4c7f7f00848ebc6abeed640e588ece65f7f
Roberto Falconi <R.Falconi@almaviva.it>
2023-01-22 16:43:19 +0100

    Refactoring

--- MinimalAPIs/Program.cs
+++ MinimalAPIs/Program.cs
@@ -14,18 +14,23 @@ using MinimalAPIs.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add loggers to the container.
+builder.Logging.AddJsonConsole();
+
+// Add configurations to the container.
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+var connectionString = builder.Configuration.GetConnectionString("connectionstring") ?? builder.Configuration["ConnectionString"]?.ToString() ?? "";          // from Secrets.json ?? from appsettings.json
 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!));
 var keyCert = new X509SecurityKey(new X509Certificate2(builder.Configuration["Certificate:Path"]!, builder.Configuration["Certificate:Password"]));
 var keys = new List<SecurityKey> { key, keyCert };
-var connectionString = builder.Configuration.GetConnectionString("connectionstring") ?? builder.Configuration["ConnectionString"]?.ToString() ?? "";          // from Secrets.json ?? from appsettings.json
 
-builder.Services.AddDbContext<MinimalDbContext>(options => options.UseSqlServer(connectionString));
+// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<MinimalDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddSwaggerGen(options =>
 {
     options.SupportNonNullableReferenceTypes();
@@ -75,14 +80,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 builder.Services.AddAuthorization();
-builder.Logging.AddJsonConsole();
 builder.Services.AddHealthChecks();
 builder.Services.AddScoped<MyTokenHandler>();
 builder.Services.AddHangfire(configuration => configuration.UseMemoryStorage()).AddHangfireServer();
 JobStorage.Current = new MemoryStorage();
 
+// Add the app to the container.
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 using (var scope = app.Services.CreateScope())
 {
     scope.ServiceProvider.GetService<MyTokenService>();
@@ -95,6 +101,10 @@ if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
 
 app.UseHsts();
 app.UseHttpsRedirection();
@@ -103,13 +113,12 @@ app.UseRouting();
 app.UseAuthorization();
 app.UseHangfireDashboard();
 app.MapHealthChecks("/healthz");
-app.UseExceptionHandler("/error");
 
-app.Map("/error", (HttpContext context) =>
+app.Map("/Error", (HttpContext context) =>
 {
     var error = context.Features.Get<IExceptionHandlerFeature>()?.Error;
     var errorMessage = error?.Message ?? "Unknown error";
-    app.Logger.LogError(error, errorMessage);
+    app.Logger.LogError(error, errorMessage.ToString());
     return Results.Json(data: errorMessage, statusCode: StatusCodes.Status400BadRequest);
 }).ExcludeFromDescription();
 
