367ec691f2a036fd2c825ab5d0f4180ce85f7dc5
Roberto Falconi <R.Falconi@almaviva.it>
2023-07-29 11:34:52 +0200

    Add POST and Create Customer

--- MinimalAPIs/Endpoints/CustomerEndpoint.cs
+++ MinimalAPIs/Endpoints/CustomerEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace MinimalAPIs.Endpoints;
+﻿using MinimalAPIs.Models.API;
+namespace MinimalAPIs.Endpoints;
 
-public class CustomerEndpoint
+public static class CustomerEndpoint
 {
+    public static void MapMyEndpoints(this WebApplication app)
+    {
+        app.MapPost("/customer", async ([FromBody] Customer customer) =>
+        {
+            var validationResult = new CustomerValidator().Validate(customer);
 
+            if (validationResult.IsValid)
+            {
+                // TODO: Add customer in DB
+                return Results.Ok();
+            }
+            else
+            {
+                return Results.BadRequest(validationResult.Errors);
+            }
+        });
+    }
 }
